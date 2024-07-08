2ece7e8c94a1154e8ae355d1bea1fa8875f6c15b
ProdamGarage111 <nata.simonenko.72@mail.ru>
2024-07-08 12:31:23 -0400

    Initial commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+class Hero:
+    def __init__(self, name, strength, agility, intelligence):
+        self.name = name
+        self.strength = strength
+        self.agility = agility
+        self.intelligence = intelligence
+
+class Enemy:
+    def __init__(self, name, strength, agility, intelligence):
+         self.name = name
+         self.strength = strength
+         self.agility = agility
+         self.intelligence = intelligence
+
+class Dungeon:
+    def __init__(self, name, size, difficulty):
+        self.name = name
+        self.size = size
+        self.difficulty = difficulty
+        
